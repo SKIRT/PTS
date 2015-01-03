09e7b13f4450d5f1f9235224feb95bf4e23f0595
Sam Verstocken <sam.verstocken@ugent.be>
2015-01-03 11:41:30 +0100

    When the constructor of the Log class is not given a report path, log messages are only printed to the terminal

--- pts/log.py
+++ pts/log.py
@@ -27,40 +27,64 @@ class Log:
     
     def __init__(self, reportpath="", reportname=""):
 
-        reportpath = os.path.realpath(os.path.expanduser(reportpath))
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
-        reportfilepath = os.path.join(reportpath, "report_" + reportname + "_" + timestamp + ".txt")
-        self._report = open(reportfilepath, 'w')
+        self._doreport = True if reportpath else False
+        if self._doreport:
+            reportpath = os.path.realpath(os.path.expanduser(reportpath))
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+            reportfilepath = os.path.join(reportpath, "report_" + reportname + "_" + timestamp + ".txt")
+            self._report = open(reportfilepath, 'w')
     
     def info(self, message):
+        
         stampedmessage = timestamp(message)
-        self._report.write(stampedmessage + "\n")
-        self._report.flush()
-        os.fsync(self._report.fileno())
+        
+        if self._doreport:
+        
+            self._report.write(stampedmessage + "\n")
+            self._report.flush()    
+            os.fsync(self._report.fileno())
+        
         print stampedmessage
 
     def warning(self, message):
+        
         stampedmessage = timestamp(message)
-        self._report.write(stampedmessage + "\n")
-        self._report.flush()
-        os.fsync(self._report.fileno())
+        
+        if self._doreport:
+        
+            self._report.write(stampedmessage + "\n")
+            self._report.flush()
+            os.fsync(self._report.fileno())
+        
         print "\033[35m" + stampedmessage + "\033[0m"
 
     def success(self, message):
+        
         stampedmessage = timestamp(message)
-        self._report.write(stampedmessage + "\n")
-        self._report.flush()
-        os.fsync(self._report.fileno())
+        
+        if self._doreport:
+        
+            self._report.write(stampedmessage + "\n")
+            self._report.flush()
+            os.fsync(self._report.fileno())
+        
         print "\033[32m" + stampedmessage + "\033[0m"
     
     def error(self, message):
+        
         stampedmessage = timestamp(message)
-        self._report.write(stampedmessage + "\n")
-        self._report.flush()
-        os.fsync(self._report.fileno())
+        
+        if self._doreport:
+            
+            self._report.write(stampedmessage + "\n")
+            self._report.flush()
+            os.fsync(self._report.fileno())
+        
         print "\033[31m" + stampedmessage + "\033[0m"
 
     def finish(self):
-        self._report.close()
+        
+        if self._doreport:
+            self._report.close()
 
 # -----------------------------------------------------------------
