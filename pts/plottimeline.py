2e2988201d1b37d805fc72bf839241b822b919a7
Sam Verstocken <sam.verstocken@ugent.be>
2015-05-06 18:26:22 +0200

    Added extracttimeline and plottimeline scripts to extract and plot an overview of the contribution of the different simulation stages to the simulation time, for every parallel process

---
+++ pts/plottimeline.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# *****************************************************************
+# **       PTS -- Python Toolkit for working with SKIRT          **
+# **       © Astronomical Observatory, Ghent University          **
+# *****************************************************************
+
+## \package pts.plottimeline Plot a timeline of a simulation, for the different parallel processes, from
+#  a text file containing the timeline information extracted from the simulation's log files.
+#
+
+# -----------------------------------------------------------------
+
+# Import standard modules
+import numpy as np
+
+# Use a non-interactive back-end to generate high-quality vector graphics
+import matplotlib
+if matplotlib.get_backend().lower() != "pdf": matplotlib.use("pdf")
+import matplotlib.pyplot as plt
+
+# Import relevant PTS modules
+from pts.log import Log
+
+# -----------------------------------------------------------------
+
+# Ignore warnings, otherwise Canopy would give a UserWarning on top of the error encountered when a progress
+# file does not contain any data (an error which is catched an produces an error message).
+import warnings
+warnings.filterwarnings("ignore")
+
+# -----------------------------------------------------------------
+
+# Define the names of the different simulation phases (for the plot's legend)
+phasenames = ['Setup', 'Stellar emission', 'Communication', 'Spectra', 'Dust emission']
+
+# Define the colors for the different simulation phases in the plot
+# (setup = red, stellar = green, comm = blue, spectra = magenta, dust = cyan)
+colors = list('rgbmc')
+
+# -----------------------------------------------------------------
+
+# Create a logger
+log = Log()
+
+## This function plots the timeline of a simulation, for the different parallel processes, from a text file
+#  containing the timeline information extracted from the simulation's log files.
+def plottimeline(filepath, plotpath, figsize=(12,8), percentages=False):
+
+    # Try to get the values from the txt file. If no data could be found in the file, we skip it.
+    try:
+        ranks, phases, starttimes, endtimes = np.loadtxt(filepath, usecols=(0,1,2,3), unpack=True)
+    except ValueError:
+        log.warning("The file " + filepath + " does not contain any data")
+        return
+
+    # Determine the number of processors
+    nprocs = int(ranks[-1]) + 1
+    procranks = range(nprocs)
+
+    # Initialize a data structure to contain the start times and endtimes for the different processes, indexed on the phase
+    data = []
+
+    # Iterate over the different rows found in the txt file and copy the start- and endtimes in the data list
+    for i in range(len(ranks)):
+
+        if int(ranks[i]) == 0:
+
+            data.append([int(phases[i]), [], []])
+            data[len(data)-1][1].append(starttimes[i])
+            data[len(data)-1][2].append(endtimes[i])
+
+        else:
+
+            nphases = len(data)
+            data[i%nphases][1].append(starttimes[i])
+            data[i%nphases][2].append(endtimes[i])
+
+    # Initialize a figure with the appropriate size
+    plt.figure(figsize=figsize)
+    ax = plt.gca()
+
+    legendEntries = []
+    legendNames = []
+    unique_phases = []
+
+    # Make the timeline plot, consisting of a set of bars of the same color for each simulation phase
+    for phase, starttimes, endtimes in data:
+
+        durations = np.array(endtimes) - np.array(starttimes)
+        patch_handle = ax.barh(procranks, durations, color=colors[phase], align='center', left=starttimes, alpha=0.8)
+
+        if phase not in unique_phases:
+
+            unique_phases.append(phase)
+            legendEntries.append(patch_handle)
+            legendNames.append(phasenames[phase])
+
+        if percentages:
+
+            for rank, patch in enumerate(patch_handle.get_children()):
+
+                bl = patch.get_xy()
+                x = 0.5*patch.get_width() + bl[0]
+                y = 0.5*patch.get_height() + bl[1]
+                ax.text(x, y, "%d%%" % (rank), ha='center')
+
+    # Format the axis ticks and labels
+    ax.set_yticks(procranks)
+    ax.set_yticklabels(procranks)
+    ax.set_xlabel('Time (s)', fontsize='large')
+    ax.set_ylabel('Process rank', fontsize='large')
+    ax.yaxis.grid(True)
+
+    # Shrink current axis's height by 10% on the bottom
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+
+    # Put a legend below current axis
+    ax.legend(legendEntries, legendNames, loc='upper center', bbox_to_anchor=(0.5, -0.10), fancybox=True, shadow=False, ncol=5)
+
+    # Save the figure
+    plt.savefig(plotpath, bbox_inches='tight', pad_inches=0.40)
+    log.info("Created PDF timeline " + plotpath)
+
+    # Close the plotting environment
+    plt.close()
+
+# -----------------------------------------------------------------
