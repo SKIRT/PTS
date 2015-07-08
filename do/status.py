f1e5a78852189967e048196df26fb00180ab3fb2
Sam Verstocken <sam.verstocken@ugent.be>
2015-07-08 15:38:58 +0200

    Use ranges or values seperated by commas for pts status --delete option

--- do/status.py
+++ do/status.py
@@ -24,6 +24,32 @@ from pts.skirtsimulation import SkirtSimulation
 
 # -----------------------------------------------------------------
 
+# This function returns a list of integer values, based on a string denoting a certain range (e.g. '3-9') or a
+# set of integer values seperated by commas ('2,14,20')
+def int_list(string):
+
+    # Split the string
+    splitted = string.split('-')
+
+    if len(splitted) == 0: raise argparse.ArgumentTypeError("No range given")
+    elif len(splitted) == 1:
+
+        splitted = splitted[0].split(",")
+
+        # Check if the values are valid
+        for value in splitted:
+            if not value.isdigit(): raise argparse.ArgumentTypeError("Argument contains unvalid characters")
+
+        # Only leave unique values
+        return list(set([int(value) for value in splitted]))
+
+    elif len(splitted) == 2:
+
+        if not (splitted[0].isdigit() and splitted[1].isdigit()): raise argparse.ArgumentTypeError("Not a valid integer range")
+        return range(int(splitted[0]), int(splitted[1])+1)
+
+    else: raise argparse.ArgumentTypeError("Values must be seperated by commas or by a '-' in the case of a range")
+
 # This private helper function returns the datetime object corresponding to the time stamp in a line
 def _timestamp(line):
 
@@ -38,7 +64,7 @@ def _timestamp(line):
 
 # Create the command-line parser
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--delete', nargs='+', type=int)
+parser.add_argument('-d', '--delete', type=int_list)
 
 # Parse the command line arguments
 args = parser.parse_args()
