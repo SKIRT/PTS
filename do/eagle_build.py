1245f1f9f12571c9ed61d5e4c16e52fc14e5f730
Peter Camps <peter.camps@ugent.be>
2015-10-27 11:28:09 +0100

    add procedures to build extra EAGLE visualization files

--- do/eagle_build.py
+++ do/eagle_build.py
@@ -7,15 +7,19 @@
 
 ## \package do.eagle_build Build visualization files for completed EAGLE SKIRT-runs.
 #
-# This script builds SED plots, RGB images, wavelength movies, or info files from the results of completed EAGLE
-# SKIRT-runs. The resulting files are placed in the corresponding \c vis directories, replacing any previous versions.
+# This script builds plots, images, movies, or info files from the results of completed EAGLE SKIRT-runs.
+# The resulting files are placed in the corresponding \c vis directories, replacing any previous versions.
 #
 # The script expects exactly two command-line arguments. The first argument specifies what type of files to build.
 # It should be one of the following strings (it is sufficient to specify an unambiguous portion of the string):
-#  - seds: a PDF plot combining the SEDs for all of the instruments
-#  - rgbimages: an optical RGB image for each of the instruments
-#  - wavemovie: a wavelength movie for the three instruments
+#  - densities: a plot with histograms of stellar, gas and dust densities in function of the galaxy radius
+#  - greybodyfit: a plot showing a modified blackbody fit to the dust continuum emission and listing the temperature
 #  - infofile: a text info file with statistics on the simulation results
+#  - particles: a 3D plot of the gas particles indicating star forming, cold and hot gas in different colors
+#  - rgbimages: an optical RGB image for each of the frame instruments
+#  - seds: a plot combining the SEDs for all of the instruments
+#  - temperature: a plot with a histogram of dust mass versus dust temperature
+#  - wavemovie: a wavelength movie for the three frame instruments
 #
 # The second argument further specifies the operation that should be performed. It should be one of the following:
 #  - the string "update" to perform the build for all completed SKIRT-runs for which the visualization files do not
@@ -35,6 +39,21 @@ import os.path
 from eagle.database import Database
 from eagle.skirtrun import SkirtRun
 from eagle.skirtrun import runids_in_range
+
+# -----------------------------------------------------------------
+
+# a list of relevant filename endings for each visualization type
+filenames_for_vistype = {
+    'densities':    ( "density_curves.pdf", ),
+    'greybodyfit':  ( "dust_body_fit.pdf", ),
+    'infofile':     ( "info.txt", ),
+    'particles':    ( "gas_particles.pdf", ),
+    'rgbimages':    ( "xy_total_optical.png", "xz_total_optical.png", "yz_total_optical.png",
+                      "xy_total_augmented.png", "xz_total_augmented.png", "yz_total_augmented.png" ),
+    'seds':         ( "sed.pdf", ),
+    'temperature':  ( "dust_temperature.pdf", ),
+    'wavemovie':    ( "wave.mov", ),
+}
 
 # -----------------------------------------------------------------
 
@@ -70,11 +89,13 @@ def move_visualization_files(skirtrun, filenames):
 if len(sys.argv) != 3: raise ValueError("This script expects exactly two command-line arguments")
 
 vistype = sys.argv[1].lower()
-sed  = "seds".startswith(vistype)
-rgb  = "rgbimages".startswith(vistype)
-wave = "wavemovie".startswith(vistype)
-info = "infofile".startswith(vistype)
-if not sed and not rgb and not wave and not info: raise ValueError("Unknown visualization type: " + vistype)
+found = False
+for knowntype in filenames_for_vistype.keys():
+    if knowntype.startswith(vistype):
+        vistype = knowntype
+        found = True
+        break
+if not found: raise ValueError("Unknown visualization type: " + vistype)
 
 buildrange = sys.argv[2].lower()
 update =  "update".startswith(buildrange)
@@ -84,14 +105,8 @@ if not update and not rebuild and not runidrange: raise ValueError("Unknown buil
 
 # -----------------------------------------------------------------
 
-# construct a list of relevant filename endings depending on the visualization type
-filenames = []
-if sed: filenames += [ "sed.pdf" ]
-if rgb: filenames += [ "xy_total_optical.png", "xz_total_optical.png", "yz_total_optical.png",
-                       "xy_total_augmented.png", "xz_total_augmented.png", "yz_total_augmented.png" ]
-if wave: filenames += [ "wave.mov" ]
-if info: filenames += [ "info.txt" ]
-filenames = tuple(filenames)
+# get a list of relevant filename endings depending on the visualization type
+filenames = filenames_for_vistype[vistype]
 
 # construct the list of SKIRT-runs to be processed
 if runidrange:
@@ -99,17 +114,51 @@ if runidrange:
 else:
     skirtruns = completed_skirtruns(filenames if update else None)
 
-# build SED plots for each SKIRT-run
-if sed:
-    from pts.plotseds import plotseds
-    print "Building SED plots for {} SKIRT-runs".format(len(skirtruns))
+# =================================================================
+
+# build density curves
+if vistype=='densities':
+    from eagle.plotdensitycurves import plotdensitycurves
+    print "Building density curves for {} SKIRT-runs".format(len(skirtruns))
     for skirtrun in skirtruns:
-        print "Building SED plot for SKIRT-run {}...".format(skirtrun.runid())
-        plotseds(skirtrun.simulation())
+        print "Building density curves for SKIRT-run {}...".format(skirtrun.runid())
+        plotdensitycurves(skirtrun)
+
+# -----------------------------------------------------------------
+
+# build grey body fits
+if vistype=='greybodyfit':
+    from eagle.plotgreybodyfit import plotgreybodyfit
+    print "Building grey body fits for {} SKIRT-runs".format(len(skirtruns))
+    for skirtrun in skirtruns:
+        print "Building grey body fit for SKIRT-run {}...".format(skirtrun.runid())
+        plotgreybodyfit(skirtrun)
+
+# -----------------------------------------------------------------
+
+# build info files
+if vistype=='infofile':
+    from eagle.makeinfofile import makeinfofile
+    print "Building info files for {} SKIRT-runs".format(len(skirtruns))
+    for skirtrun in skirtruns:
+        print "Building info file for SKIRT-run {}...".format(skirtrun.runid())
+        makeinfofile(skirtrun)
         move_visualization_files(skirtrun, filenames)
 
-# build RGB images for each SKIRT-run
-if rgb:
+# -----------------------------------------------------------------
+
+# build gas particle plots
+if vistype=='particles':
+    from eagle.plotgasparticles import plotgasparticles
+    print "Building gas particle plots for {} SKIRT-runs".format(len(skirtruns))
+    for skirtrun in skirtruns:
+        print "Building gas particle plot for SKIRT-run {}...".format(skirtrun.runid())
+        plotgasparticles(skirtrun)
+
+# -----------------------------------------------------------------
+
+# build RGB images
+if vistype=='rgbimages':
     from pts.makergbimages import makeintegratedrgbimages
     from pts.filter import Filter
     print "Building RGB images for {} SKIRT-runs".format(len(skirtruns))
@@ -127,22 +176,36 @@ if rgb:
             postfix="_augmented", fmin=fmin, fmax=fmax)
         move_visualization_files(skirtrun, filenames)
 
+# -----------------------------------------------------------------
+
+# build SED plots for each SKIRT-run
+if vistype=='seds':
+    from pts.plotseds import plotseds
+    print "Building SED plots for {} SKIRT-runs".format(len(skirtruns))
+    for skirtrun in skirtruns:
+        print "Building SED plot for SKIRT-run {}...".format(skirtrun.runid())
+        plotseds(skirtrun.simulation())
+        move_visualization_files(skirtrun, filenames)
+
+# -----------------------------------------------------------------
+
+# build temperature histograms
+if vistype=='temperature':
+    from eagle.plottemperature import plottemperature
+    print "Building temperature histograms for {} SKIRT-runs".format(len(skirtruns))
+    for skirtrun in skirtruns:
+        print "Building temperature histogram for SKIRT-run {}...".format(skirtrun.runid())
+        plottemperature(skirtrun)
+
+# -----------------------------------------------------------------
+
 # build wavelength movies for each SKIRT-run
-if wave:
+if vistype=='wavemovie':
     from pts.makewavemovie import makewavemovie
     print "Building wavelength movies for {} SKIRT-runs".format(len(skirtruns))
     for skirtrun in skirtruns:
         print "Building wavelength movie for SKIRT-run {}...".format(skirtrun.runid())
         makewavemovie(skirtrun.simulation())
-        move_visualization_files(skirtrun, filenames)
-
-# build info files for each SKIRT-run
-if info:
-    from eagle.makeinfofile import makeinfofile
-    print "Building info files for {} SKIRT-runs".format(len(skirtruns))
-    for skirtrun in skirtruns:
-        print "Building info file for SKIRT-run {}...".format(skirtrun.runid())
-        makeinfofile(skirtrun)
         move_visualization_files(skirtrun, filenames)
 
 print "Done..."
