fa9c5b4f11569e38e23552a8a74a7391a4a11e49
Sam Verstocken <sam.verstocken@ugent.be>
2015-11-14 15:01:23 +0100

    Added launcher module with SkirtLauncher, FitSkirtLauncher and SkirtMemoryLauncher classes

--- do/launch.py
+++ do/launch.py
@@ -10,6 +10,9 @@
 
 # -----------------------------------------------------------------
 
+# Ensure Python 3 compatibility
+from __future__ import absolute_import, division, print_function
+
 # Import standard modules
 import os
 import argparse
@@ -25,15 +28,17 @@ parser.add_argument("file", type=str, help="the name of the ski/fski file")
 parser.add_argument("inpath", type=str, help="the simulation input path")
 parser.add_argument("outpath", type=str, help="the simulation output path")
 parser.add_argument("--memory", action="store_true", help="run the memory console application")
-parser.add_argument("--relative", action="treats the given input and output paths as being relative to the ski/fski file")
-parser.add_argument('--brief', action='store_true', help="enable brief console logging")
-parser.add_argument('--verbose', action='store_true', help="enable verbose logging mode for SKIRT")
-parser.add_argument('--plotseds', action='store_true', help="make plots of the output SEDs")
-parser.add_argument('--plotgrids', action='store_true', help="make plots of the dust grid")
-parser.add_argument('--plotprogress', action='store_true', help="make plots of the progress of the different processes as a function of time")
-parser.add_argument('--plottimeline', action='store_true', help="make a plot of the timeline for the different processes")
-parser.add_argument('--makergb', action='store_true', help="add this option to make RGB images from the SKIRT output")
-parser.add_argument('--makewave', action='store_true', help="add this option to make a wave movie from the SKIRT output")
+parser.add_argument("--relative", action="store_true", help="treats the given input and output paths as being relative to the ski/fski file")
+parser.add_argument("--brief", action="store_true", help="enable brief console logging")
+parser.add_argument("--verbose", action="store_true", help="enable verbose logging mode")
+parser.add_argument("--logmemory", action="store_true", help="enable memory logging mode")
+parser.add_argument("--plotseds", action="store_true", help="make plots of the output SEDs")
+parser.add_argument("--plotgrids", action="store_true", help="make plots of the dust grid")
+parser.add_argument("--plotprogress", action="store_true", help="make plots of the progress of the different processes as a function of time")
+parser.add_argument("--plottimeline", action="store_true", help="make a plot of the timeline for the different processes")
+parser.add_argument("--plotmemory", action="store_true", help="make a plot of the memory consumption as a function of time")
+parser.add_argument("--makergb", action="store_true", help="add this option to make RGB images from the SKIRT output")
+parser.add_argument("--makewave", action="store_true", help="add this option to make a wave movie from the SKIRT output")
 
 # Parse the command line arguments
 arguments = parser.parse_args()
@@ -47,7 +52,7 @@ arguments.filepath = os.path.abspath(arguments.file)
 arguments.input_path = os.path.abspath(arguments.inpath)
 arguments.output_path = os.path.abspath(arguments.outpath)
 
-exit()
+# -----------------------------------------------------------------
 
 # If the parameter file describes a SKIRT simulation
 if arguments.filepath.endswith(".ski"):
@@ -57,7 +62,7 @@ if arguments.filepath.endswith(".ski"):
 
         # Create a SkirtMemoryLauncher instance and run it
         launcher = SkirtMemoryLauncher.from_arguments(arguments)
-        launcher.run(filepath, input_path, output_path)
+        launcher.run()
 
     else:
 
