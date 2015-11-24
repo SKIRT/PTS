f420c5d635f926b341871bbaa28d4b7f86090ace
Sam Verstocken <sam.verstocken@ugent.be>
2015-11-24 16:25:06 +0100

    Put most of the files in the right directory

--- magic/__init__.py
+++ magic/__init__.py
@@ -3,3 +3,10 @@
 # *****************************************************************
 # **       Astromagic -- the image editor for Astronomers        **
 # *****************************************************************
+
+# Import classes to make them available at the level of this subpackage
+from galaxyextraction import GalaxyExtractor
+from starextraction import StarExtractor
+from skyextraction import SkyExtractor
+from sextractor import SExtractor
+from wcsfinder import WCSFinder
