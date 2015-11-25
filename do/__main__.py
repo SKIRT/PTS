cb3ccbdcb8777592b9ce9ce16ffe76bc6efaff4a
Sam Verstocken <sam.verstocken@ugent.be>
2015-11-25 13:36:36 +0100

    Fixed calling pts from the terminal

--- do/__main__.py
+++ do/__main__.py
@@ -28,39 +28,97 @@
 
 # -----------------------------------------------------------------
 
-# Ensure Python 3 compatibility
-from __future__ import absolute_import, division, print_function
-
 # Import standard modules
 import sys
 import os.path
+from operator import itemgetter
 
-# get a sorted list of the names (without extension) of all python scripts in the directory containing this file
+# -----------------------------------------------------------------
+
+# Get the path to the 'do' directory
 path = os.path.dirname(sys.argv[0])
-names = filter(lambda fn: fn.endswith(".py") and not fn.startswith("_"), os.listdir(path))
-names = [ name[:-3] for name in sorted(names) ]
 
-# get a list of the script names that match the first command line argument, if there is one;
-# if the argument doesn't match any script names, try again with the portion of the script names after an underscore
-if len(sys.argv) > 1:
-    matches = filter(lambda name: name.startswith(sys.argv[1]), names)
-    if len(matches)==0:
-        matches = filter(lambda name: '_' in name and name.split('_',1)[1].startswith(sys.argv[1]), names)
-else:
+# Loop over the directories within the 'do' subpackage
+scripts = []
+for item in os.listdir(path):
+
+    # Get the full path to the item
+    item_path = os.path.join(path, item)
+
+    # Skip items that are not directories
+    if not os.path.isdir(item_path): continue
+
+    # Loop over the files in the directory
+    for name in os.listdir(item_path):
+
+        # Get the full name to the file
+        file_path = os.path.join(item_path, name)
+
+        # Skip items that are not files
+        if not os.path.isfile(file_path): continue
+
+        # Add the file path
+        if file_path.endswith(".py") and "__init__" not in file_path: scripts.append((item, name))
+
+# Sort the script names
+scripts = sorted(scripts, key=itemgetter(1))
+
+# Get the name of the script to execute
+script_name = sys.argv[1] if len(sys.argv) > 1 else None
+
+# Get a list of the script names that match the first command line argument, if there is one
+if "/" in script_name:
+
     matches = []
+    dir_name = script_name.split("/")[0]
+    script_name = script_name.split("/")[1]
 
-# if there is a unique match, execute the script
+    # Loop over all found items
+    for item in scripts:
+        if item[0] == dir_name and item[1].startswith(script_name): matches.append(item)
+
+elif script_name is not None: matches = filter(lambda item: item[1].startswith(script_name), scripts)
+else: matches = []
+
+# If there is a unique match, execute the script
 # (after adjusting the command line arguments so that it appears that the script was executed directly)
-if len(matches)==1:
+if len(matches) == 1:
     target = os.path.join(path, matches[0] + ".py")
     sys.argv[0] = target
     del sys.argv[1]
-    print("Executing: " + matches[0] + " " + " ".join(sys.argv[1:]))
+    print "Executing: " + matches[0] + " " + " ".join(sys.argv[1:])
     exec open(target)
 
-# if not, print the list of available scripts
+elif len(matches) == 0:
+
+    print "No match found. Available scripts:"
+
+    # Sort on the 'do' subfolder name
+    scripts = sorted(scripts, key=itemgetter(0))
+
+    current_dir = None
+    for script in scripts:
+
+        if current_dir == script[0]:
+            print " "*len(current_dir) + "/" + script[1]
+        else:
+            print script[0] + "/" + script[1]
+            current_dir = script[0]
+
 else:
-    print("Available scripts:")
-    for name in names: print("  " + name)
+
+    print "The command you provided is ambiguous. Possible matches:"
+
+    # Sort on the 'do' subfolder name
+    matches = sorted(matches, key=itemgetter(0))
+
+    current_dir = None
+    for script in matches:
+
+        if current_dir == script[0]:
+            print " "*len(current_dir) + "/" + script[1]
+        else:
+            print script[0] + "/" + script[1]
+            current_dir = script[0]
 
 # -----------------------------------------------------------------
