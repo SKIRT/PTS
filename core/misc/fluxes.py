Trying to fix the normalization of the stellar components

core/misc/fluxes.py
@@ -17,6 +17,7 @@ import numpy as np
 
 # Import astronomical modules
 from astropy.units import Unit, spectral_density
+from astropy import constants
 
 # Import the relevant PTS classes and modules
 from ..tools import tables, filesystem
@@ -147,8 +148,15 @@ class ObservedFluxCalculator(object):
             wavelengths = sed.wavelengths("micron", asarray=True)
             fluxdensities = []
             for wavelength, fluxdensity_jy in zip(sed.wavelengths("micron"), sed.fluxes("Jy")):
-                fluxdensity = fluxdensity_jy.to("W / (m2 * micron)", equivalencies=spectral_density(wavelength))
+
+                # 2 different ways should be the same:
+                fluxdensity_ = fluxdensity_jy.to("W / (m2 * micron)", equivalencies=spectral_density(wavelength))
+                fluxdensity = fluxdensity_jy.to("W / (m2 * Hz)").value * spectral_factor_hz_to_micron(wavelength) * Unit("W / (m2 * micron)")
+
+                print(fluxdensity_, fluxdensity)
+
                 fluxdensities.append(fluxdensity.to("W / (m2 * micron)").value)
+
             fluxdensities = np.array(fluxdensities) # in W / (m2 * micron)
 
             # Loop over the different filters
@@ -188,5 +196,55 @@ class ObservedFluxCalculator(object):
 
             # Write out the flux table
             tables.write(self.tables[name], path)
+
+# -----------------------------------------------------------------
+
+# The speed of light
+speed_of_light = constants.c
+
+# -----------------------------------------------------------------
+
+def spectral_factor_hz_to_micron(wavelength):
+
+    """
+    This function ...
+    :param wavelength:
+    :return:
+    """
+
+    wavelength_unit = "micron"
+    frequency_unit = "Hz"
+
+    # Convert string units to Unit objects
+    if isinstance(wavelength_unit, basestring): wavelength_unit = Unit(wavelength_unit)
+    if isinstance(frequency_unit, basestring): frequency_unit = Unit(frequency_unit)
+
+    conversion_factor_unit = wavelength_unit / frequency_unit
+
+    # Calculate the conversion factor
+    factor = (wavelength ** 2 / speed_of_light).to(conversion_factor_unit).value
+    return 1. / factor
+
+# -----------------------------------------------------------------
+
+def spectral_factor_hz_to_meter(wavelength):
+
+    """
+    This function ...
+    :return:
+    """
+
+    wavelength_unit = "m"
+    frequency_unit = "Hz"
+
+    # Convert string units to Unit objects
+    if isinstance(wavelength_unit, basestring): wavelength_unit = Unit(wavelength_unit)
+    if isinstance(frequency_unit, basestring): frequency_unit = Unit(frequency_unit)
+
+    conversion_factor_unit = wavelength_unit / frequency_unit
+
+    # Calculate the conversion factor
+    factor = (wavelength ** 2 / speed_of_light).to(conversion_factor_unit).value
+    return 1./factor
 
 # -----------------------------------------------------------------

modeling/core/sed.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 
 # Import astronomical modules
-from astropy.units import Unit
+from astropy.units import Unit, spectral
 from astropy.table import Table
 
 # Import the relevant PTS classes and modules
@@ -25,10 +25,79 @@ from ...core.basics.errorbar import ErrorBar
 
 # -----------------------------------------------------------------
 
+class IntrinsicSED(object):
+
+    """
+    This class ...
+    """
+
+    def __init__(self):
+
+        """
+        This function ...
+        """
+
+        # Attributes
+        self.table = None
+
+    # -----------------------------------------------------------------
+
+    def wavelengths(self, unit=None, asarray=False, add_unit=True):
+
+        """
+        This function ...
+        :param unit:
+        :param asarray:
+        :param add_unit:
+        :return:
+        """
+
+        if asarray: return tables.column_as_array(self.table["Wavelength"], unit=unit)
+        else: return tables.column_as_list(self.table["Wavelength"], unit=unit, add_unit=add_unit)
+
+    # -----------------------------------------------------------------
+
+    def luminosities(self, unit=None, asarray=False, add_unit=True):
+
+        """
+        This function ...
+        :param unit:
+        :param asarray:
+        :param add_unit:
+        :return:
+        """
+
+        if asarray: return tables.column_as_array(self.table["Luminosity"], unit=unit)
+        else: return tables.column_as_list(self.table["Luminosity"], unit=unit, add_unit=add_unit)
+
+    # -----------------------------------------------------------------
+
+    @classmethod
+    def from_file(cls, path, skiprows=0):
+        """
+        This function ...
+        :param path:
+        :param skiprows:
+        :return:
+        """
+
+        # Create a new SED
+        sed = cls()
+
+        wavelength_column, luminosity_column = np.loadtxt(path, dtype=float, unpack=True, skiprows=skiprows)
+        sed.table = tables.new([wavelength_column, luminosity_column], ["Wavelength", "Luminosity"])
+        sed.table["Wavelength"].unit = Unit("micron")
+        sed.table["Luminosity"].unit = Unit("W/micron")
+
+        # Return the SED
+        return sed
+
+# -----------------------------------------------------------------
+
 class ObservedSED(object):
 
     """
-    This function ...
+    This class ...
     """
 
     def __init__(self):
@@ -381,11 +450,12 @@ class SED(object):
     # -----------------------------------------------------------------
 
     @classmethod
-    def from_file(cls, path):
+    def from_file(cls, path, skiprows=0):
 
         """
         This function ...
         :param path:
+        :param skiprows:
         :return:
         """
 
@@ -403,7 +473,7 @@ class SED(object):
         #sed.table.rename_column("col1", "Wavelength")
         #sed.table.rename_column("col2", "Flux")
 
-        wavelength_column, flux_column = np.loadtxt(path, dtype=float, unpack=True)
+        wavelength_column, flux_column = np.loadtxt(path, dtype=float, unpack=True, skiprows=skiprows)
         sed.table = tables.new([wavelength_column, flux_column], ["Wavelength", "Flux"])
         sed.table["Wavelength"].unit = Unit("micron")
 
@@ -412,11 +482,12 @@ class SED(object):
         for i in range(len(sed.table)):
 
             # Get the flux density in W / m2 and the wavelength in micron
-            neutral_fluxdensity = sed.table["Flux"][i]
+            neutral_fluxdensity = sed.table["Flux"][i] * Unit("W/m2")
             wavelength = sed.table["Wavelength"][i] * Unit("micron")
 
-            # Convert to Jansky
-            jansky = unitconversion.neutral_fluxdensity_to_jansky(neutral_fluxdensity, wavelength)
+            # Convert to Jansky (2 methods give same result)
+            #jansky_ = unitconversion.neutral_fluxdensity_to_jansky(neutral_fluxdensity, wavelength)
+            jansky = (neutral_fluxdensity / wavelength.to("Hz", equivalencies=spectral())).to("Jy").value
 
             # Add the fluxdensity in Jansky to the new column
             jansky_column.append(jansky)