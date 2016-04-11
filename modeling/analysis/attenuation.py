943cff360e921aaaa8a95da3050fe934ddd15cc9
Sam Verstocken <sam.verstocken@ugent.be>
2016-04-11 15:17:08 +0200

    Added placeholders for future model analysis classes

---
+++ modeling/analysis/attenuation.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# *****************************************************************
+# **       PTS -- Python Toolkit for working with SKIRT          **
+# **       © Astronomical Observatory, Ghent University          **
+# *****************************************************************
+
+## \package pts.modeling.analysis.attenuation Contains the AttenuationAnalyser class
+
+# -----------------------------------------------------------------
+
+# Ensure Python 3 compatibility
+from __future__ import absolute_import, division, print_function
+
+# Import the relevant PTS classes and modules
+from .component import AnalysisComponent
+
+# -----------------------------------------------------------------
+
+# IR-through-UV extinction curve (Fitzpatrick+, 2007):
+# http://vizier.cfa.harvard.edu/viz-bin/VizieR?-source=J/ApJ/663/320
+#  - J/ApJ/663/320/stars: Survey stars (tables 1, 3 and 4 of paper) (328 rows)
+
+# UVOT imaging of M81 and Holmberg IX (Hoversten+, 2011):
+#  - J/AJ/141/205/table3: Source photometry and fitted parameters
+#    http://vizier.cfa.harvard.edu/viz-bin/VizieR-3?-source=J/AJ/141/205/table3
+#  -> has "AV": Best fitting internal extinction A_V
+
+class AttenuationAnalyser(AnalysisComponent):
+    
+    """
+    This class...
+    """
+
+    def __init__(self, config=None):
+
+        """
+        The constructor ...
+        :param config:
+        :return:
+        """
+
+        # Call the constructor of the base class
+        super(AttenuationAnalyser, self).__init__(config)
+
+    # -----------------------------------------------------------------
+
+    @classmethod
+    def from_arguments(cls, arguments):
+
+        """
+        This function ...
+        :param arguments:
+        :return:
+        """
+
+        pass
+
+    # -----------------------------------------------------------------
+
+    def run(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+        # 1. Call the setup function
+        self.setup()
+
+# -----------------------------------------------------------------
---
+++ modeling/analysis/heating.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# *****************************************************************
+# **       PTS -- Python Toolkit for working with SKIRT          **
+# **       © Astronomical Observatory, Ghent University          **
+# *****************************************************************
+
+## \package pts.modeling.analysis.heating Contains the DustHeatingAnalyser class.
+
+# -----------------------------------------------------------------
+
+# Ensure Python 3 compatibility
+from __future__ import absolute_import, division, print_function
+
+# Import the relevant PTS classes and modules
+from .component import AnalysisComponent
+
+# -----------------------------------------------------------------
+
+class DustHeatingAnalyser(AnalysisComponent):
+    
+    """
+    This class...
+    """
+
+    def __init__(self, config=None):
+
+        """
+        The constructor ...
+        :param config:
+        :return:
+        """
+
+        # Call the constructor of the base class
+        super(DustHeatingAnalyser, self).__init__(config)
+
+    # -----------------------------------------------------------------
+
+    @classmethod
+    def from_arguments(cls, arguments):
+
+        """
+        This function ...
+        :param arguments:
+        :return:
+        """
+
+        pass
+
+    # -----------------------------------------------------------------
+
+    def run(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+        # 1. Call the setup function
+        self.setup()
+
+# -----------------------------------------------------------------
