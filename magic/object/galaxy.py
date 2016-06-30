7ded58738f22c56f3a5426150ee415315eed05f8
Sam Verstocken <sam.verstocken@ugent.be>
2016-06-30 11:44:59 +0200

    Small fix that ensures the old behaviour is kept when the orientation angle is a standard one (0, 90, 180..)

--- magic/object/galaxy.py
+++ magic/object/galaxy.py
@@ -103,7 +103,8 @@ class Galaxy(SkyObject):
         :return:
         """
 
-        orientation = wcs.orientation_angle
+        try: orientation = wcs.standard_orientation_angle
+        except ValueError: orientation = wcs.orientation_angle
 
         # Add the orientation angle (w.r.t. standard E-W and S-N projection on the x and y axes) to the position angle
         # that is expressed in the standard way
