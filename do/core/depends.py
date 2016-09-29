3e0b351e10622d8a75bd5e5b4e0ed751ee0a5aee
Sam Verstocken <sam.verstocken@ugent.be>
2016-09-29 14:22:34 +0200

    Added option to pts depends to check which packages are available trough the Canopy package manager

--- do/core/depends.py
+++ do/core/depends.py
@@ -18,6 +18,7 @@ import argparse
 from collections import defaultdict
 
 # Import the relevant PTS classes and modules
+from pts.do.commandline import show_all_available, show_possible_matches
 from pts.core.tools import introspection
 from pts.core.tools.logging import log
 from pts.core.tools import filesystem as fs
@@ -30,6 +31,9 @@ parser.add_argument("script", type=str, nargs="?", help="the name of the PTS do 
 parser.add_argument("-m", "--modules", action="store_true", help="show the PTS modules which import a given package")
 parser.add_argument("-s", "--standard", action="store_true", help="show import packages from the python standard library")
 parser.add_argument("-v", "--version", action="store_true", help="show the version numbers of the required packages")
+parser.add_argument("-c", "--canopy", action="store_true", help="show whether the package is available through Canopy")
+parser.add_argument("-p", "--pip", action="store_true", help="show whether the package is available through pip") # not working yet
+parser.add_argument("-d", "--description", action="store_true", help="show a description") # not working yet
 
 # Parse the command line arguments
 arguments = parser.parse_args()
@@ -45,7 +49,7 @@ else:
     scripts = introspection.get_scripts()
     tables = introspection.get_arguments_tables()
 
-    # Find matching 'do' commands (actuall scripts or tabulated commands)
+    # Find matching 'do' commands (actual scripts or tabulated commands)
     matches = introspection.find_matches_scripts(arguments.script, scripts)
     table_matches = introspection.find_matches_tables(arguments.script, tables)
 
@@ -54,12 +58,12 @@ else:
 
     # No match
     if len(matches) + len(table_matches) == 0:
-        introspection.show_all_available(scripts, tables)
+        show_all_available(scripts, tables)
         exit()
 
     # More mathces
     elif len(matches) + len(table_matches) > 1:
-        introspection.show_possible_matches(matches, table_matches, tables)
+        show_possible_matches(matches, table_matches, tables)
         exit()
 
     # Exactly one match from existing do script
@@ -102,6 +106,41 @@ else:
 # Get the names and versions of all installed python packages
 packages = introspection.installed_python_packages()
 
+# Get the names of the packages available through Canopy
+if arguments.canopy:
+
+    url = "https://www.enthought.com/products/canopy/package-index/"
+
+    import requests
+    from lxml import html
+
+    page_as_string = requests.get(url).content
+    tree = html.fromstring(page_as_string)
+
+    tables = [e for e in tree.iter() if e.tag == 'table']
+    table = tables[-1]
+
+    table_rows = [e for e in table.iter() if e.tag == 'tr']
+    column_headings = [e.text_content() for e in table_rows[0].iter() if e.tag == 'th']
+
+    table_rows = [e for e in table.iter() if e.tag == 'tr']
+    column_headings = [e.text_content() for e in table_rows[0].iter() if e.tag == 'th']
+
+    # return table_rows, column_headings
+
+    #for row in table_rows: print(row.text_content())
+
+    #objname = str(table_rows[1].text_content().split("\n")[1]).strip()
+
+    canopy_packages = []
+
+    for row in table_rows[1:]:
+
+        name = row[0].text_content().strip()
+        canopy_packages.append(name.lower())
+
+else: canopy_packages = None
+
 # Loop over the packages and report their presence
 for dependency in sorted(dependencies, key=str.lower):
 
@@ -119,15 +158,23 @@ for dependency in sorted(dependencies, key=str.lower):
         present = introspection.is_present_package(dependency)
         version = None
 
+    # Check whether the package is available through Canopy
+    in_canopy = dependency.lower() in canopy_packages if canopy_packages is not None else None
+    in_canopy_string = " <<CANOPY>>" if in_canopy else ""
+
+    # Check whether the package is available through pip
+    #if arguments.pip:
+    #else: pip = None
+
     # Check whether the current package is present
     if present:
 
         # Show package name, whether it's present and version number (if requested)
-        if version is not None: log.success(dependency + ": present (version " + version + ")")
-        else: log.success(dependency + ": present")
+        if version is not None: log.success(dependency + ": present (version " + version + ")" + in_canopy_string)
+        else: log.success(dependency + ": present" + in_canopy_string)
 
     # The package is not present
-    else: log.error(dependency + ": not found")
+    else: log.error(dependency + ": not found" + in_canopy_string)
 
     # List the PTS modules that have this dependency
     if arguments.modules:
