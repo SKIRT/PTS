f1a2d1ce51c5f08c29a71290e31bc54cbd0cc225
Sam Verstocken <sam.verstocken@ugent.be>
2017-02-01 11:49:57 +0100

    Small changes

---
+++ core/test/test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# *****************************************************************
+# **       PTS -- Python Toolkit for working with SKIRT          **
+# **       © Astronomical Observatory, Ghent University          **
+# *****************************************************************
+
+## \package pts.core.test.test Contains the PTSTest class.
+
+# -----------------------------------------------------------------
+
+# Ensure Python 3 compatibility
+from __future__ import absolute_import, division, print_function
+
+# -----------------------------------------------------------------
+
+class PTSTest(object):
+
+    """
+    This class ...
+    """
+
+    def __init__(self):
+
+        """
+        This function ...
+        """
+
+    # -----------------------------------------------------------------
+
+    def run(self):
+
+        """
+        This function ...
+        """
+
+        # Setup
+        self.setup()
+
+        # Perform
+        self.perform()
+
+        # Check
+        self.check()
+
+        # Show
+        self.show()
+
+        # Write
+        self.write()
+
+    # -----------------------------------------------------------------
+
+    def setup(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+    # -----------------------------------------------------------------
+
+    def perform(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+    # -----------------------------------------------------------------
+
+    def check(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+    # -----------------------------------------------------------------
+
+    def show(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+    # -----------------------------------------------------------------
+
+    def write(self):
+
+        """
+        This function ...
+        :return:
+        """
+
+# -----------------------------------------------------------------
