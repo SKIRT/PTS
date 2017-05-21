8bd8ac306085a8fd4f9cf2205eb1e87654daea09
Sam Verstocken <sam.verstocken@ugent.be>
2017-05-21 17:52:58 +0200

    Removed unused stuff

--- do/core/invert.py
+++ do/core/invert.py
@@ -11,34 +11,33 @@
 
 # Import standard modules
 import imageio
-import argparse
 
 # Import the relevant PTS classes and modules
 from pts.core.basics.rgbimage import invert_colors
 from pts.core.tools import filesystem as fs
+from pts.core.basics.configuration import ConfigurationDefinition, ArgumentConfigurationSetter
 
 # -----------------------------------------------------------------
 
-# Create the command-line parser
-parser = argparse.ArgumentParser()
-
-# Basic options
-parser.add_argument("filename", type=str, help="the name of the input image file")
+# Create the definition
+definition = ConfigurationDefinition()
+definition.add_required("filename", "file_path", "name of the input image file")
 
 # Parse the command line arguments
-arguments = parser.parse_args()
+setter = ArgumentConfigurationSetter("invert")
+config = setter.run(definition)
 
 # -----------------------------------------------------------------
 
 # Open the original image
-image = imageio.imread(arguments.filename)
+image = imageio.imread(config.filename)
 
 # Invert the colours
 invert_colors(image)
 
 # Determine output name
-name = fs.strip_extension(fs.name(arguments.filename))
-extension = fs.get_extension(arguments.filename)
+name = fs.strip_extension(fs.name(config.filename))
+extension = fs.get_extension(config.filename)
 newname = name + "_inverted." + extension
 
 # Write the inverted image
