3288efc679876a2b3595072db0fe7f1e7c14754c
Sam Verstocken <sam.verstocken@ugent.be>
2018-08-20 17:47:46 +0200

    Fixed a problem with network.exists()

--- core/tools/network.py
+++ core/tools/network.py
@@ -24,11 +24,12 @@ from . import progress
 
 # -----------------------------------------------------------------
 
-def exists(url):
+def exists(url, allow_redirection=False):
 
     """
     This function ...
     :param url:
+    :param allow_redirection:
     :return:
     """
 
@@ -36,11 +37,72 @@ def exists(url):
 
     p = urlparse(url)
     conn = httplib.HTTPConnection(p.netloc)
-    conn.request('HEAD', p.path)
+
+    try: conn.request('HEAD', p.path)
+    except: return False
+
     resp = conn.getresponse()
     conn.close()
-    # alternative: return response.status == 200
-    return resp.status < 400
+
+    #print(resp.status)
+
+    # successful
+    # OK = 200
+    # CREATED = 201
+    # ACCEPTED = 202
+    # NON_AUTHORITATIVE_INFORMATION = 203
+    # NO_CONTENT = 204
+    # RESET_CONTENT = 205
+    # PARTIAL_CONTENT = 206
+    # MULTI_STATUS = 207
+    # IM_USED = 226
+    #
+    # # redirection
+    # MULTIPLE_CHOICES = 300
+    # MOVED_PERMANENTLY = 301
+    # FOUND = 302
+    # SEE_OTHER = 303
+    # NOT_MODIFIED = 304
+    # USE_PROXY = 305
+    # TEMPORARY_REDIRECT = 307
+    #
+    # # client error
+    # BAD_REQUEST = 400
+    # UNAUTHORIZED = 401
+    # PAYMENT_REQUIRED = 402
+    # FORBIDDEN = 403
+    # NOT_FOUND = 404
+    # METHOD_NOT_ALLOWED = 405
+    # NOT_ACCEPTABLE = 406
+    # PROXY_AUTHENTICATION_REQUIRED = 407
+    # REQUEST_TIMEOUT = 408
+    # CONFLICT = 409
+    # GONE = 410
+    # LENGTH_REQUIRED = 411
+    # PRECONDITION_FAILED = 412
+    # REQUEST_ENTITY_TOO_LARGE = 413
+    # REQUEST_URI_TOO_LONG = 414
+    # UNSUPPORTED_MEDIA_TYPE = 415
+    # REQUESTED_RANGE_NOT_SATISFIABLE = 416
+    # EXPECTATION_FAILED = 417
+    # UNPROCESSABLE_ENTITY = 422
+    # LOCKED = 423
+    # FAILED_DEPENDENCY = 424
+    # UPGRADE_REQUIRED = 426
+    #
+    # # server error
+    # INTERNAL_SERVER_ERROR = 500
+    # NOT_IMPLEMENTED = 501
+    # BAD_GATEWAY = 502
+    # SERVICE_UNAVAILABLE = 503
+    # GATEWAY_TIMEOUT = 504
+    # HTTP_VERSION_NOT_SUPPORTED = 505
+    # INSUFFICIENT_STORAGE = 507
+    # NOT_EXTENDED = 510
+
+    # Allow redirection?
+    if allow_redirection: return resp.status < 400
+    else: return resp.status < 300
 
 # -----------------------------------------------------------------
 
