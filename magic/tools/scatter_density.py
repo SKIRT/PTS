8c7cec3c745652175fae395e2badd62d08c3ed9e
Sam Verstocken <sam.verstocken@ugent.be>
2018-12-07 15:34:43 +0100

    Fixed missing class

--- magic/tools/scatter_density.py
+++ magic/tools/scatter_density.py
@@ -16,93 +16,13 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.projections import register_projection
-from math import ceil
+from math import ceil, log10
 from matplotlib.image import AxesImage, _interpd_, _image
 from matplotlib.transforms import IdentityTransform, TransformedBbox, BboxTransformFrom, Bbox, Affine2D
 import matplotlib.colors as mcolors
 
-# -----------------------------------------------------------------
-
-__all__ = ['GenericDensityArtist', 'ScatterDensityArtist', 'ScatterDensityAxes']
-
-# -----------------------------------------------------------------
-
-class ScatterDensityArtist(GenericDensityArtist):
-
-    """
-    Matplotlib artist to make a density plot of (x, y) scatter data.
-
-    Parameters
-    ----------
-    ax : `matplotlib.axes.Axes`
-        The axes to plot the artist into.
-    x, y : iterable
-        The data to plot.
-    c : iterable
-        Values to use for color-encoding. This is meant to be the same as
-        the argument with the same name in :meth:`~matplotlib.axes.Axes.scatter`
-        although for now only 1D iterables of values are accepted. Note that
-        values are averaged inside each pixel of the density map *before*
-        applying the colormap, which in some cases will be different from what
-        the average color of markers would have been inside each pixel.
-    dpi : int or `None`
-        The number of dots per inch to include in the density map. To use
-        the native resolution of the drawing device, set this to None.
-    downres_factor : int
-        For interactive devices, when panning, the density map will
-        automatically be made at a lower resolution and including only a
-        subset of the points. The new dpi of the figure when panning will
-        then be dpi / downres_factor, and the number of elements in the
-        arrays will be reduced by downres_factor**2.
-    cmap : `matplotlib.colors.Colormap`
-        The colormap to use for the density map.
-    color : str or tuple
-        The color to use for the density map. This can be any valid
-        Matplotlib color. If specified, this takes precedence over the
-        colormap.
-    alpha : float
-        Overall transparency of the density map.
-    norm : `matplotlib.colors.Normalize`
-        The normalization class for the density map.
-    vmin, vmax : float or func
-        The lower and upper levels used for scaling the density map. These can
-        optionally be functions that take the density array and returns a single
-        value (e.g. a function that returns the 5% percentile, or the minimum).
-        This is useful since when zooming in/out, the optimal limits change.
-    update_while_panning : bool, optional
-        Whether to compute histograms on-the-fly while panning.
-    kwargs
-        Any additional keyword arguments are passed to AxesImage.
-    """
-
-    def __init__(self, ax, x, y, downres_factor=4, c=None, **kwargs):
-        self.histogram2d_helper = FixedDataDensityHelper(ax, x, y, c=c, downres_factor=downres_factor)
-        super(ScatterDensityArtist, self).__init__(ax, histogram2d_func=self.histogram2d_helper, **kwargs)
-
-    # -----------------------------------------------------------------
-
-    def set_xy(self, x, y):
-        self.histogram2d_helper.set_xy(x, y)
-
-    # -----------------------------------------------------------------
-
-    def set_c(self, c):
-        self.histogram2d_helper.set_c(c)
-
-    # -----------------------------------------------------------------
-
-    def on_press(self, event=None, force=False):
-        if not force:
-            if self._update_while_panning and self.histogram2d_helper._downres_factor == 1:
-                return
-        self.histogram2d_helper.downres()
-        return super(ScatterDensityArtist, self).on_press(force=force)
-
-    # -----------------------------------------------------------------
-
-    def on_release(self, event=None):
-        self.histogram2d_helper.upres()
-        return super(ScatterDensityArtist, self).on_release()
+# Import other modules
+from fast_histogram import histogram2d
 
 # -----------------------------------------------------------------
 
@@ -797,5 +717,210 @@ def alpha_from_densities(densities, tresfrac=0.05, scaleval=10., logscale=True):
 
     # Return alpha
     return alpha
+
+# -----------------------------------------------------------------
+
+class FixedDataDensityHelper:
+
+    compute_when_pressed = True
+
+    def __init__(self, ax, x, y, c=None, downres_factor=4):
+
+        self._ax = ax
+        self._c = None
+        self._downres = False
+
+        if downres_factor < 1 or downres_factor % 1 != 0:
+            raise ValueError('downres_factor should be a strictly positive integer value')
+
+        self._downres_factor = downres_factor
+        self.set_xy(x, y)
+        self.set_c(c)
+
+    def downres(self):
+        self._downres = True
+
+    def upres(self):
+        self._downres = False
+
+    def set_xy(self, x, y):
+        self._x = x
+        self._y = y
+        self._x_log = None
+        self._y_log = None
+        self._x_log_sub = None
+        self._y_log_sub = None
+        step = self._downres_factor ** 2
+        self._x_sub = self._x[::step]
+        self._y_sub = self._y[::step]
+
+    def set_c(self, c):
+        self._c = c
+        step = self._downres_factor ** 2
+        if self._c is None:
+            self._c_sub = None
+        else:
+            self._c_sub = self._c[::step]
+
+    def _update_x_log(self):
+        step = self._downres_factor ** 2
+        with np.errstate(invalid='ignore'):
+            self._x_log = np.log10(self._x)
+        self._x_log_sub = self._x_log[::step]
+
+    def _update_y_log(self):
+        step = self._downres_factor ** 2
+        with np.errstate(invalid='ignore'):
+            self._y_log = np.log10(self._y)
+        self._y_log_sub = self._y_log[::step]
+
+    def __call__(self, bins=None, range=None, return_density=False):
+
+        ny, nx = bins
+        (ymin, ymax), (xmin, xmax) = range
+
+        xscale = self._ax.get_xscale()
+        yscale = self._ax.get_yscale()
+
+        if xscale == 'log':
+            xmin, xmax = log10(xmin), log10(xmax)
+            if self._x_log is None:
+                # We do this here insead of in set_xy to save time since in
+                # set_xy we don't know yet if the axes will be log or not.
+                self._update_x_log()
+            if self._downres:
+                x = self._x_log_sub
+            else:
+                x = self._x_log
+        elif xscale == 'linear':
+            if self._downres:
+                x = self._x_sub
+            else:
+                x = self._x
+        else:  # pragma: nocover
+            raise ValueError('Unexpected xscale: {0}'.format(xscale))
+
+        if yscale == 'log':
+            ymin, ymax = log10(ymin), log10(ymax)
+            if self._y_log is None:
+                # We do this here insead of in set_xy to save time since in
+                # set_xy we don't know yet if the axes will be log or not.
+                self._update_y_log()
+            if self._downres:
+                y = self._y_log_sub
+            else:
+                y = self._y_log
+        elif yscale == 'linear':
+            if self._downres:
+                y = self._y_sub
+            else:
+                y = self._y
+        else:  # pragma: nocover
+            raise ValueError('Unexpected xscale: {0}'.format(xscale))
+
+        if self._downres:
+            nx_sub = nx // self._downres_factor
+            ny_sub = ny // self._downres_factor
+            bins = (ny_sub, nx_sub)
+            weights = self._c_sub
+        else:
+            bins = (ny, nx)
+            weights = self._c
+
+        if weights is None:
+            array = histogram2d(y, x, bins=bins,
+                                range=((ymin, ymax), (xmin, xmax)))
+            density = array / np.nanmax(array)
+        else:
+            array = histogram2d(y, x, bins=bins, weights=weights,
+                                range=((ymin, ymax), (xmin, xmax)))
+            count = histogram2d(y, x, bins=bins,
+                                range=((ymin, ymax), (xmin, xmax)))
+            density = count / np.nanmax(count)
+
+            with np.errstate(invalid='ignore'):
+                array /= count
+
+        if return_density: return array, density
+        else: return array
+
+# -----------------------------------------------------------------
+
+class ScatterDensityArtist(GenericDensityArtist):
+
+    """
+    Matplotlib artist to make a density plot of (x, y) scatter data.
+
+    Parameters
+    ----------
+    ax : `matplotlib.axes.Axes`
+        The axes to plot the artist into.
+    x, y : iterable
+        The data to plot.
+    c : iterable
+        Values to use for color-encoding. This is meant to be the same as
+        the argument with the same name in :meth:`~matplotlib.axes.Axes.scatter`
+        although for now only 1D iterables of values are accepted. Note that
+        values are averaged inside each pixel of the density map *before*
+        applying the colormap, which in some cases will be different from what
+        the average color of markers would have been inside each pixel.
+    dpi : int or `None`
+        The number of dots per inch to include in the density map. To use
+        the native resolution of the drawing device, set this to None.
+    downres_factor : int
+        For interactive devices, when panning, the density map will
+        automatically be made at a lower resolution and including only a
+        subset of the points. The new dpi of the figure when panning will
+        then be dpi / downres_factor, and the number of elements in the
+        arrays will be reduced by downres_factor**2.
+    cmap : `matplotlib.colors.Colormap`
+        The colormap to use for the density map.
+    color : str or tuple
+        The color to use for the density map. This can be any valid
+        Matplotlib color. If specified, this takes precedence over the
+        colormap.
+    alpha : float
+        Overall transparency of the density map.
+    norm : `matplotlib.colors.Normalize`
+        The normalization class for the density map.
+    vmin, vmax : float or func
+        The lower and upper levels used for scaling the density map. These can
+        optionally be functions that take the density array and returns a single
+        value (e.g. a function that returns the 5% percentile, or the minimum).
+        This is useful since when zooming in/out, the optimal limits change.
+    update_while_panning : bool, optional
+        Whether to compute histograms on-the-fly while panning.
+    kwargs
+        Any additional keyword arguments are passed to AxesImage.
+    """
+
+    def __init__(self, ax, x, y, downres_factor=4, c=None, **kwargs):
+        self.histogram2d_helper = FixedDataDensityHelper(ax, x, y, c=c, downres_factor=downres_factor)
+        super(ScatterDensityArtist, self).__init__(ax, histogram2d_func=self.histogram2d_helper, **kwargs)
+
+    # -----------------------------------------------------------------
+
+    def set_xy(self, x, y):
+        self.histogram2d_helper.set_xy(x, y)
+
+    # -----------------------------------------------------------------
+
+    def set_c(self, c):
+        self.histogram2d_helper.set_c(c)
+
+    # -----------------------------------------------------------------
+
+    def on_press(self, event=None, force=False):
+        if not force:
+            if self._update_while_panning and self.histogram2d_helper._downres_factor == 1:
+                return
+        self.histogram2d_helper.downres()
+        return super(ScatterDensityArtist, self).on_press(force=force)
+
+    # -----------------------------------------------------------------
+
+    def on_release(self, event=None):
+        self.histogram2d_helper.upres()
+        return super(ScatterDensityArtist, self).on_release()
 
 # -----------------------------------------------------------------
